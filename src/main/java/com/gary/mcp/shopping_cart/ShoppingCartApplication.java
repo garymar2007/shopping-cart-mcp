9d1305cf6e46508bd9dbc2fb1da1c7914ae44724
garyma <garymar2007@gmail.com>
2025-09-25 22:09:50 +0200

    Added bean to register shoppingCartTool, Added claude desktop server config json file.

--- src/main/java/com/gary/mcp/shopping_cart/ShoppingCartApplication.java
+++ src/main/java/com/gary/mcp/shopping_cart/ShoppingCartApplication.java
@@ -1,7 +1,13 @@
 package com.gary.mcp.shopping_cart;
 
+import com.gary.mcp.shopping_cart.tools.ShoppingCartMcpService;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ShoppingCartApplication {
@@ -9,5 +15,10 @@ public class ShoppingCartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartApplication.class, args);
 	}
+
+    @Bean
+    public List<ToolCallback> shoppingCartToolCallback(ShoppingCartMcpService shoppingCartMcpService) {
+        return List.of(ToolCallbacks.from(shoppingCartMcpService));
+    }
 
 }
